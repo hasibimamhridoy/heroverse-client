8b20da83a017cfa16c28985519e73275165b32f3
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-05-18 05:10:17 +0600

    tab layout section working a primary data loaded

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,18 @@
+import BannerSlider from "../BannerSlider/BannerSlider";
+import Gallary from "../Gallary/Gallary";
+import MyTabs from "../Tabs/MyTabs";
 
-import BannerSlider from '../BannerSlider/BannerSlider';
-import Gallary from '../Gallary/Gallary';
+
 
 
 const Home = () => {
-    return (
-        <div>
-            <BannerSlider></BannerSlider>
-         <Gallary></Gallary>
-        </div>
-    );
+  return (
+    <div>
+      <BannerSlider></BannerSlider>
+      <Gallary></Gallary>
+     <MyTabs></MyTabs>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/Tabs/MyTabs.jsx
@@ -0,0 +1,225 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  console.log(value);
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+export default function MyTabs() {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(() => {
+    fetch(`http://localhost:5000/products?cateId=${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [value]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <AppBar sx={{ bgcolor: "black" }} position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab sx={{ color: "white" }} label="Item One" {...a11yProps(0)} />
+          <Tab sx={{ color: "white" }} label="Item Two" {...a11yProps(1)} />
+          <Tab sx={{ color: "white" }} label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <div className="h-fit text-white">
+            <div>
+              {products.map((product) => {
+                const {
+                  category_id,
+                  description,
+                  name,
+                  picture,
+                  price,
+                  quantity,
+                  rating,
+                  seller_email,
+                  seller_name,
+                  sub_category,
+                  _id,
+                } = product;
+                return (
+                  <div key={_id}>
+                    <h1>{name}</h1>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <div className="h-fit text-white">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-5 lg:gap-7">
+              {products.map((product) => {
+                const {
+                  category_id,
+                  description,
+                  name,
+                  picture,
+                  price,
+                  quantity,
+                  rating,
+                  seller_email,
+                  seller_name,
+                  sub_category,
+                  _id,
+                } = product;
+                return (
+                  <div key={_id}>
+                    <div className="w-full max-w-sm bg-purple-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                      <a href="#">
+                        <img
+                          className=" rounded-t-lg"
+                          src={picture}
+                          alt="product image"
+                        />
+                      </a>
+                      <div className="px-5 pb-5">
+                        <a href="#">
+                          <h5 className="text-xl font-semibold text-white  tracking-tight  dark:text-white">
+                            Apple Watch Series 7 GPS, Aluminium Case, Starlight
+                            Sport
+                          </h5>
+                        </a>
+                        <div className="flex items-center mt-2.5 mb-5">
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>First star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Second star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Third star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Fourth star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            className="w-5 h-5 text-yellow-300"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Fifth star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                            5.0
+                          </span>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                            $599
+                          </span>
+                          <a
+                            href="#"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          >
+                            Add to cart
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}></TabPanel>
+      </SwipeableViews>
+    </Box>
+  );
+}
